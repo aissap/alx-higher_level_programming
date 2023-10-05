dc64e401e93c4b1842d7069fd60a0ec42061c1e0
aissap <jesus.jesus.core@gmail.com>
2023-10-05 18:14:21 -0400

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def print_args():
     argc = len(sys.argv) - 1
 
-    print("{} argument{}{}".format(argc, "s" if argc != 1 else "", ":" if argc else "."))
+    print("{} argument{}{}".format(
+        argc, "s" if argc != 1 else "", ":" if argc else "."))
 
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
+
 
 if __name__ == "__main__":
     print_args()
