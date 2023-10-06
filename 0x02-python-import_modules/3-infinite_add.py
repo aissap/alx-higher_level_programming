b4bf3bc9f63c0e89856896f0a74dc35c9dcc04d1
aissap <jesus.jesus.core@gmail.com>
2023-10-05 20:15:39 -0400

    :hidden:

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 def add_all(*args):
-
     total = 0
     for arg in args:
         total += int(arg)
     return total
 
+
 if __name__ == "__main__":
+
     import sys
 
     total = add_all(*sys.argv[1:])
+
     print("{}".format(total))
