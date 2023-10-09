46cfa185cd65f70531d51f0fa52afe8d1e06300d
aissap <jesus.jesus.core@gmail.com>
2023-10-09 18:32:07 -0400

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,7 @@ def divisible_by_2(my_list=[]):
         my_list (list): The list of integers.
 
     Returns:
-        list of bool: A new list containing True for multiples of 2, and False otherwise.
+        list of bool: A new list containing True for multiples of 2, and False.
     """
     multiples = [num % 2 == 0 for num in my_list]
     return multiples
