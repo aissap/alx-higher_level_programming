d32f9527756da62f467df68ce8073dcfcd056f42
aissap <jesus.jesus.core@gmail.com>
2023-10-09 12:20:37 -0400

    maxi

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    Finds the largest integer in a list.
+
+    Args:
+        my_list (list): The list of integers.
+
+    Returns:
+        int or None: The largest integer in the list, or None if the list is empty.
+    """
+    if not my_list:
+        return None
+
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
