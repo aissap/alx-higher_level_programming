11e3f005af46f49619a575a28750d4466c1a9255
aissap <jesus.jesus.core@gmail.com>
2023-10-09 21:26:19 -0400

    c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,18 +6,20 @@
  *
  * Return: A pointer to the head of the reversed list.
  */
-listint_t *reverse_listint(listint_t **head) {
-    listint_t *node = *head, *next, *prev = NULL;
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
 
-    while (node) {
-        next = node->next;
-        node->next = prev;
-        prev = node;
-        node = next;
-    }
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
 
-    *head = prev;
-    return *head;
+	*head = prev;
+	return (*head);
 }
 
 /**
@@ -27,39 +29,44 @@ listint_t *reverse_listint(listint_t **head) {
  * Return: If the linked list is not a palindrome - 0.
  *         If the linked list is a palindrome - 1.
  */
-int is_palindrome(listint_t **head) {
-    listint_t *tmp, *rev, *mid;
-    size_t size = 0, i;
+int is_palindrome(listint_t **head)
+{
+	istint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return 1;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    tmp = *head;
-    while (tmp) {
-        size++;
-        tmp = tmp->next;
-    }
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
 
-    tmp = *head;
-    for (i = 0; i < (size / 2) - 1; i++)
-        tmp = tmp->next;
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-    if ((size % 2) == 0 && tmp->n != tmp->next->n)
-        return 0;
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
 
-    tmp = tmp->next->next;
-    rev = reverse_listint(&tmp);
-    mid = rev;
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
 
-    tmp = *head;
-    while (rev) {
-        if (tmp->n != rev->n)
-            return 0;
-        tmp = tmp->next;
-        rev = rev->next;
-    }
+	tmp = *head;
 
-    reverse_listint(&mid);
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
 
-    return 1;
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+
+	reverse_listint(&mid);
+
+	return (1);
 }
