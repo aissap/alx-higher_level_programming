7fe1009a7a16551cab7260974bde0d81e1eceabf
aissap <jesus.jesus.core@gmail.com>
2023-10-11 19:18:40 -0400

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+a = 1
+b = 2
+from add_0 import add
 
-if __name__ == "__main__":
-    from add_0 import add
+result = add(a, b)
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+print(f"{a} + {b} = {result}")
