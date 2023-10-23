dfddc924a990e7d71d13ef135d1b85705856b146
aissap <jesus.jesus.core@gmail.com>
2023-10-23 19:20:28 -0400

    safe-print list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     index = 0
     try:
-        for i in range(x):
+        for i in range(0, x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 index += 1
