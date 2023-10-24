5cc714072cff85987c6d73f1b724d0da4bf4b0d7
aissap <jesus.jesus.core@gmail.com>
2023-10-24 19:52:00 -0400

    2-square

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -3,11 +3,11 @@ Square = __import__('2-square').Square
 
 my_square_1 = Square(3)
 print(type(my_square_1))
-print(my_square_1._dict_)
+print(my_square_1.__dict__)
 
 my_square_2 = Square()
 print(type(my_square_2))
-print(my_square_2._dict_)
+print(my_square_2.__dict__)
 
 try:
     print(my_square_1.size)
@@ -22,13 +22,13 @@ except Exception as e:
 try:
     my_square_3 = Square("3")
     print(type(my_square_3))
-    print(my_square_3._dict_)
+    print(my_square_3.__dict__)
 except Exception as e:
     print(e)
 
 try:
     my_square_4 = Square(-89)
     print(type(my_square_4))
-    print(my_square_4._dict_)
+    print(my_square_4.__dict__)
 except Exception as e:
     print(e)
