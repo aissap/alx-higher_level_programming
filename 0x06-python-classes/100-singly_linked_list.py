40866b3ba96549fdd769d7e65112d8ac7f13af4d
aissap <jesus.jesus.core@gmail.com>
2023-10-24 22:17:47 -0400

    100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Define classes for SinglyLinkedList"""
+"""Defines singly-linked list that """
 
 
 class Node:
-    """Defines  a node in a singly-linked list."""
-
+    """ class Node in singly-linked list """
     def __init__(self, data, next_node=None):
-        """Initializes the new Node.
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
@@ -27,8 +20,7 @@ class Node:
 
     @property
     def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
@@ -38,36 +30,32 @@ class Node:
 
 
 class SinglyLinkedList:
+    """ Initializes a new node into the correct sorted position
+    in the list"""
     def __init__(self):
-        """Initializes a new instance of the SinglyLinkedList class."""
-        self.head = None
+        self.__head = None
 
     def sorted_insert(self, value):
-        """
-        Inserts a new Node into the correct sorted position in the list .
-
-        Args:
-            value: The new Node to insert.
-        """
-        new_node = Node(value)
-
-        if self.head is None or value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
-            return
-
-        current = self.head
-        while current.next_node is not None and current.next_node.data < value:
-            current = current.next_node
-
-        new_node.next_node = current.next_node
-        current.next_node = new_node
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
     def __str__(self):
-        """Returns a string representation of the list."""
-        result = ""
-        current = self.head
-        while current is not None:
-            result += str(current.data) + "\n"
-            current = current.next_node
-        return result.strip()
+        """returns a string representation of the list"""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ("\n".join(values))
