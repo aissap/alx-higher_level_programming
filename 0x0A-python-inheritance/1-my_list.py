a1983f6562c179edff53aed75ee2cf197eec56f8
aissap <jesus.jesus.core@gmail.com>
2023-11-06 15:57:05 -0500

    my-list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an inherited list class"""
+
+class MyList(list):
+    """
+    A class that inherits from list.
+    """
+
+    def print_sorted(self):
+        """
+        Print the list in sorted (ascending) order.
+        """
+        print(sorted(self))
