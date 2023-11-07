08a1a728bacfea1a05259b150e3ffbb21e74ca8b
aissap <jesus.jesus.core@gmail.com>
2023-11-07 18:48:25 -0500

    11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Initialize a Student.
+        Args:
+            first_name : The first name of the student.
+            last_name : The last name of the student.
+            age : The age of the student.
+        """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs
+                    if hasattr(self, attr)
+                    }
+    def reload_from_json(self, json):
+        """Replace attributes of the Student.
+        Args:
+            json: The key/value pairs to replace attributes with.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
