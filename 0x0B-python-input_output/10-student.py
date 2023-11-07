0350b914a61d32bc379b19a6ebe659063f24b01b
aissap <jesus.jesus.core@gmail.com>
2023-11-07 18:24:21 -0500

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,7 +18,7 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) 
-                    for attr in attrs 
+            return {attr: getattr(self, attr)
+                    for attr in attrs
                     if hasattr(self, attr)
-            }
+                    }
