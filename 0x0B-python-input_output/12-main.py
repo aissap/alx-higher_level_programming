151debc1f67e145945c3fa682ac92babb135d85f
aissap <jesus.jesus.core@gmail.com>
2023-11-07 21:55:44 -0500

    triangle

--- 0x0B-python-input_output/12-main.py
+++ 0x0B-python-input_output/12-main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """12-main"""
-
-
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
 
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,21 +3,16 @@
 
 
 def pascal_triangle(n):
-    """Check if n is less than or equal to 0"""
+
     if n <= 0:
-        """Return an empty list if n is not a positive integer"""
         return []
 
-    """Initialize the triangle"""
     triangle = [[1]]
-
-    """Generate the remaining rows"""
-    while len(triangles) != n:
-        t = triangles[-1]
-        row = []
+    while len(triangle) != n:
+        t = triangle[-1]
+        row = [1]
         for j in range(len(t) - 1):
             row.append(t[j] + t[j + 1])
         row.append(1)
         triangle.append(row)
-        """Return the resulting Pascal's triangle"""
     return (triangle)
