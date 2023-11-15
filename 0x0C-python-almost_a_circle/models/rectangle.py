619526b5df2fbfa562590f44e74ba0d266ca028e
aissap <jesus.jesus.core@gmail.com>
2023-11-14 19:15:28 -0500

    str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,9 @@ class Rectangle(Base):
         '''display the rectangle with # characters.'''
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        '''the __str__ method represents the object as a string.'''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height
+        )
