fc3320e1b32dacc47687606b89f18d4b8bd00473
aissap <jesus.jesus.core@gmail.com>
2024-01-08 19:27:30 -0500

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  return parseInt(a) + parseInt(b);
+	return parseInt(a) + parseInt(b);
 }
 
 const num1 = process.argv[2];
 const num2 = process.argv[3];
-
 const result = add(num1, num2);
 
 console.log(result || 'NaN');
