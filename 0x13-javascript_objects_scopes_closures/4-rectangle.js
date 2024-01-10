25d25fedd869af2bbc37725c83ae4a152599f0d5
aissap <jesus.jesus.core@gmail.com>
2024-01-10 18:16:54 -0500

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,22 +1,33 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
       const empty = Object.create(Rectangle.prototype);
       return empty;
     } else {
-      this.width = w;
-      this.height = h;
+      this.width = width;
+      this.height = height;
     }
   }
 
-  print () {
+  print() {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
       }
     }
+  }
+
+  rotate() {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
