0887331b04de611b86bc5333e17ff5b3e4cc8b9f
aissap <jesus.jesus.core@gmail.com>
2024-01-10 17:43:37 -0500

    fixing

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      return Object.create(null);
+      return Object.create(Rectangle.prototype);
     } else {
       this.width = w;
       this.height = h;
