12c57e64a01c745318f53c33bc57f95f94df6578
aissap <jesus.jesus.core@gmail.com>
2024-01-10 19:36:49 -0500

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const numOccurrences = parseInt(process.argv[2]);
 
 if (isNaN(numOccurrences) || numOccurrences <= 0) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < numOccurrences; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
 }
