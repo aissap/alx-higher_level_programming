ec50b927c9b648aac84b0f3f709382275178195f
aissap <jesus.jesus.core@gmail.com>
2024-01-10 19:39:44 -0500

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-function factorial(n) {
-	if (isNaN(n) || n < 0) {
-		return 1;
-	}
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
 
-	if (n === 0 || n === 1) {
-		return 1;
-	}
+  if (n === 0 || n === 1) {
+    return 1;
+  }
 
-	return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 const num = process.argv[2];
