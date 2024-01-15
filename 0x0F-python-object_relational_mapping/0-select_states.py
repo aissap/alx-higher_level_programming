e8f39895bae87683b692420a8f7d4306ed22c807
aissap <jesus.jesus.core@gmail.com>
2024-01-14 19:55:57 -0500

    selact states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
+"""
+Script that connects to a MySQL server and lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    username, password, database = sys.argv[1:4]
 
     try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database
-                )
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
     except MySQLdb.Error as e:
         print("Error connecting to MySQL server:", e)
-        sys.exit(1)
 
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
