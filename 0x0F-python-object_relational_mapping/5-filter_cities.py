312fd8ac44da70167297c93e5c9532a8bb765034
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-19 13:48:20 -0800

    Fixing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,31 +2,30 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = database.cursor()
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    
     state_name = sys.argv[4]
 
     query = """
             SELECT cities.name
             FROM cities
-            JOIN states ON cities.state_id = states.id
+            JOIN states ON states.id = cities.state_id
             WHERE states.name = %s
             ORDER BY cities.id ASC
             """
 
-    cursor.execute(query, (state_name,))
-    result_rows = cursor.fetchall()
+    cur.execute(query, (state_name,))
+    rows = cur.fetchall()
 
     # Extract city names from the result rows
-    cities_list = [row[0] for row in result_rows]
+    cities_list = [row[0] for row in rows]
 
     # Format the output as a comma-separated string
     cities_str = ', '.join(cities_list)
     print(cities_str)
 
-    cursor.close()
-    database.close()
+    cur.close()
+    db.close()
