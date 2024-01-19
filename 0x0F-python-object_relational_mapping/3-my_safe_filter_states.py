810bfb86f556a0d2d8b81d48466ff6ec6462dcc2
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-19 11:45:04 -0800

    Fixing

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,13 +3,13 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = database.cursor()
     search_term = sys.argv[4]
-    cursor.execute("SELECT DISTINCT * FROM states WHERE name LIKE %s ORDER BY id ASC", (search_term, ))
+    cursor.execute("SELECT DISTINCT * FROM states \
+            WHERE name = %s ORDER BY id ASC", (search_term, ))
     result_rows = cursor.fetchall()
     for row in result_rows:
         print(row)
