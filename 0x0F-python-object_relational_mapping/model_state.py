801895b559fb84bfa2bb9fe9850723e32fb4366d
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-19 15:19:46 -0800

    fixing

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-mydata = MetaData()
 Base = declarative_base()
+
 
 class State(Base):
     """
@@ -13,4 +13,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
