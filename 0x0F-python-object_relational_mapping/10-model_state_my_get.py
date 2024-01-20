dba2d2e0e03848fbaa4e694717fee183ca4d2355
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-19 16:28:04 -0800

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_search = argv[4]
+    result_state = (
+        session.query(State.id)
+        .filter(State.name == state_to_search)
+        .first()
+    )
+
+    print(result_state.id) if result_state else print("Not found")
+
+    session.close()
