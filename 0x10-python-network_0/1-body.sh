7f0ad570ef234c3052bf0db8b11a7be4847d6ad8
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-25 13:38:20 -0800

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of the response for a 200 status code
 
-curl -sL -w "%{http_code}" "$1" -o temp_response.txt | tail -n +2
+curl -sX GET "$1" -L 200
