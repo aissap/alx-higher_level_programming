a79dc6bd89e7544b4de41bd57623fef4fa120356
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-26 15:29:10 -0800

    hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
 
-    print("Body response:\n"
-      f"\t- type: {type(body)}\n"
-      f"\t- content: {body}\n"
-      f"\t- utf8 content: {body.decode('utf-8')}")
+    print("Body response:\n")
+    print(f"\t- type: {type(body)}\n")
+    print(f"\t- content: {body}\n")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
