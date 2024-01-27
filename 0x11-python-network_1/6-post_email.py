d93d6d05905d7f400714358ae50f5de80b52b5c6
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-26 18:35:58 -0800

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,23 +2,13 @@
 """
 Sends an email address to a URL using a POST request and prints the response.
 """
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-    # Get the URL and email from command-line arguments
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    # Prepare the email payload
-    payload = {'email': email}
-
-    # Send a POST request to the URL with the email payload
-    response = requests.post(url, data=payload)
-
-    # Print the email address
-    print("Your email is:", email)
-
-    # Print the response body
-    print(response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
