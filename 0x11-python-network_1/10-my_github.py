f788b89e9c81b80544ec9c0e14025ea649af62f2
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-26 17:53:28 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script uses Basic Authentication with a personal access token to access GitHub API and display the user's id.
+Use Basic Authentication with a personal access token to access GitHub API.
 """
 import requests
 import sys
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Send a GET request to the GitHub API with Basic Authentication
     response = requests.get('https://api.github.com/user',
                             auth=(username, password))
-    
+
     # Check if the request is successful
     if response.status_code == 200:
         print(response.json()['id'])
