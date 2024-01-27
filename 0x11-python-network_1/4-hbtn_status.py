eb7b44dde0ee29b9fc038dd8816ab6ea646b1c6b
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-26 16:44:06 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script fetches https://alx-intranet.hbtn.io/status using the requests package.
+Fetches https://alx-intranet.hbtn.io/status using the requests package.
 """
 import requests
 
@@ -8,10 +8,10 @@ import requests
 if __name__ == "__main__":
     # Define the URL to fetch
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     # Send a GET request to the URL
     response = requests.get(url)
-    
+
     # Print the response body
     print("Body response:")
     print("\t- type:", type(response.text))
