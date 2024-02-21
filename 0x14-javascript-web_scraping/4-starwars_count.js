8d408e71074ff1ecafc03034a89dbcfead417c26
aissap <jesus.jesus.core@gmail.com>
2024-02-21 01:51:46 +0100

    fixing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,17 @@
 /* Star Wars Films Count: Prints the number of movies where character
 "Wedge Antilles" appears. Uses the Star Wars API.
 */
+
 const request = require('request');
 
-const url = process.argv[2];
-const characterId = '18';
-
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const films = JSON.parse(body).results;
-    const filmsWithAntilles = films.filter(film =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(filmsWithAntilles.length);
-  } else {
-    console.error('Error:', error || `Status code ${response.statusCode}`);
+    const wedgeAntillesFilms = films.reduce((count, film) => {
+      return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+        ? count + 1
+        : count;
+    }, 0);
+    console.log(wedgeAntillesFilms);
   }
 });
